b06c962c8c9acd1228a113ac8469519170042641
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-05-22 00:38:47 +0200

    add the inital page after making a user

---
+++ pages/controlPanel/dashboard/doctors/_phone_number/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="getDoctor-component">
+        <div class="getDoctor-content"  :class="language+'-controlPanel-padding'">
+            hey {{this.$route.params.phone_number}}
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: "dashboard",
+    middleware: "admin-auth",
+      computed: {
+    language() {
+      return this.$store.getters.getLocale;
+    },
+  },
+  mounted(){
+      const doc = this.$route.params.phone_number;
+      console.log(doc);
+  }
+}
+</script>
