0d5823b0518cc9a7bf9216a6790d3e7f7ceaf7de
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-05-26 09:20:56 +0200

    finish getUser UI

---
+++ components/forms/showUser.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="showUser-form">
+    <div class="showUserForm-content">
+        <div class="userData">
+      <div class="userImage">
+        <img class="userPic" src="http://localhost:5000/images/fawzi.png" />
+      </div>
+      <div class="userInfo">
+        <span class="user_name">Fawzi Essam Abdulfattah</span>
+        <div class="section">
+               <span style="display:block" class="weight "><i class="icon fas fa-weight"></i> 140 {{$t('dashboard.forms.allUsers.kg')}}</span>
+                 <span  style="display:block" class="height"><i class="icon fas fa-ruler-combined"></i> 180 {{$t('dashboard.forms.allUsers.cm')}}</span>
+        </div>
+         <div class="section">
+               <span style="display:block" class="birthdata ">  <i class="icon far fa-calendar-alt"></i> 20/3/1999</span>
+                 <span  style="display:block" class="height"><i class="icon fas fa-phone-square-alt"></i> 01090243795</span>
+        </div>
+          <div class="section">
+                <div class="submit-div">
+          <clinicSubmit
+            color="red"
+            :statement="$t('dashboard.forms.allUsers.blockUser')"
+            @click="editDoctor"
+          />
+        </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import clinicSubmit from "~/components/shared/clinicSubmit";
+
+export default {
+    components:{
+        clinicSubmit,
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.showUserForm-content {
+    margin-top: 60px;
+  padding: 10px;
+  display: flex;
+  flex-flow: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  .userData {
+        box-shadow: 0 5px 25px rgba(6, 150, 54, 0.233);
+        // box-shadow: 10px 15px 20px  rgba(223, 53, 53, 0.103);
+        padding: 30px;
+  display: flex;
+  flex-flow: row;
+  justify-content: space-evenly;
+  align-items: center;
+width: 50%;
+ .userImage {
+    .userPic {
+      width: 200px;
+      height: 200px;
+      border-radius: 50%;
+    }
+  }
+  .userInfo {
+      width: 100%;
+      text-align: center;
+    .user_name {
+        font-size: 20px;
+        font-weight: 600;
+        color: $main-color;
+    }
+    .section{
+        width: 100%;
+        color: black;
+        display: flex;
+        justify-content: space-evenly;
+        font-size: 20px;
+        margin-top: 16px;
+        .icon{ 
+color: $light-color;
+        }
+    }
+  }
+  }
+}
+.submit-div{
+    width:80%
+}
+</style>
--- pages/controlPanel/dashboard/users/all/index.vue
+++ pages/controlPanel/dashboard/users/all/index.vue
@@ -1,56 +1,88 @@
 <template>
-    <section class="allUsers-page">
-        <div class="allUsers-content" :class="language+'-controlPanel-padding'">
-          <div class="add-title">{{$t("dashboard.forms.allUsers.title")}}</div>
-
-              <div class="doctorError" v-if="error">
-      <notfication color="red" :label="error" />
-    </div>
-        <div class v-if="getSuccess">
-      <notfication class="doctorSuccess" color="green" :label="getSuccess" />
-    </div>
-
-          <client-only><editUser/></client-only>
+  <section class="allUsers-page">
+    <div class="allUsers-content" :class="language+'-controlPanel-padding'">
+      <div class="add-title">{{$t("dashboard.forms.allUsers.title")}}</div>
+      <!-- errors -->
+      <div class="doctorError" v-if="error">
+        <notfication color="red" :label="error" />
+      </div>
+      <div class v-if="getSuccess">
+        <notfication class="doctorSuccess" color="green" :label="getSuccess" />
+      </div>
+ <!-- search bar -->
+      <div class="search-bar">
+        <div class="input-div">
+          <clinicInput
+            :placeholder="$t('dashboard.forms.allUsers.searchHolder')"
+            @input="setSearch"
+            v-model="searchQuery"
+          ></clinicInput>
         </div>
-    </section>
+        <div class="submit-div">
+          <clinicSubmit
+            color="blue"
+            :statement="$t('dashboard.forms.allUsers.submitSearch')"
+            @click="addDoctor"
+          />
+        </div>
+      </div>
+ <!-- content -->
+   <div class="userProfile">
+        <showUser />
+      </div>
+    </div>
+  </section>
 </template>
-
-
 <script>
 import notfication from "~/components/shared/notfication";
-import editUser from "~/components/forms/editUser";
+import clinicInput from "~/components/shared/clinicInput";
+import clinicSubmit from "~/components/shared/clinicSubmit";
+import showUser from "~/components/forms/showUser";
 
 export default {
   layout: "dashboard",
   middleware: "admin-auth",
-    head() {
+
+  data() {
     return {
-      title:
-        this.language == "ar"
-          ? "المستخدمين"
-          : "Users"
+      searchQuery: "",
+      filteredData: ""
     };
   },
-  components:{
-    notfication,
-    editUser
+  head() {
+    return {
+      title: this.language == "ar" ? "المستخدمين" : "Users"
+    };
   },
-  mounted(){
+  components: {
+    notfication,
+    showUser,
+    clinicInput,
+    clinicSubmit,
+    showUser
+  },
+  methods: {
+    setSearch(value) {
+      this.searchQuery = value;
+      const searched = this.searchQuery;
+    }
+  },
+  mounted() {
     console.log("called");
-    this.$store.commit("controlPanel/doctors/setDeleteDoctorSuccess","");
+    this.$store.commit("controlPanel/doctors/setDeleteDoctorSuccess", "");
   },
   computed: {
     language() {
       return this.$store.getters.getLocale;
     },
-        error() {
+    error() {
       return this.$store.getters["controlPanel/doctors/getDeleteError"];
     },
     getSuccess() {
-      this.shouldUpdate +=1;
+      this.shouldUpdate += 1;
       return this.$store.getters["controlPanel/doctors/getDeleteSuccess"];
-    },
-  },
+    }
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -60,7 +92,7 @@ export default {
 .allUsers-content {
   width: 100%;
 }
-.add-title{
+.add-title {
   width: 100%;
   font-size: 30px;
   text-align: center;
@@ -68,7 +100,25 @@ export default {
   border-bottom: 2px solid $light-color;
   color: $black;
 }
-.doctorError, .doctorSuccess {
-margin-top:10px;
+.doctorError,
+.doctorSuccess {
+  margin-top: 10px;
+}
+.input-div {
+  width: 200px;
+}
+.submit-div {
+  width: 100px;
+}
+.search-bar {
+  margin-top: 30px;
+  display: flex;
+  flex-flow: row;
+  width: 350px;
+  align-items: center;
+  justify-content: space-between;
+}
+.userProfile {
+  width: 100%;
 }
 </style>
